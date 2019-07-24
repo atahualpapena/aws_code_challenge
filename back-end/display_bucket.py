19481c2fc51a4712fdec84b7bfa8779cd18f72c5
Atahualpa Pena <atahualpapena@thegoat.local>
2019-07-24 01:06:51 -0700

    modified display function, added function to infra

--- back-end/display_bucket.py
+++ back-end/display_bucket.py
@@ -7,4 +7,6 @@ s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     response = s3.list_buckets()
-    print(response['Buckets'][0]['Name'])
+    list = response['Buckets'][0]['Name']
+    buckets = json.dumps(list)
+    return buckets
